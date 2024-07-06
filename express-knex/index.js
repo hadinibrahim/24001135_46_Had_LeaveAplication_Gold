fcb85d70bc957c7d2aa46ddcbb809692083c9e8a
Hadin <hadinibrahim2803@gmail.com>
2024-07-06 23:11:01 +0700

    Penambahan file di folder views dan syntax di index

--- express-knex/index.js
+++ express-knex/index.js
@@ -5,6 +5,17 @@ const db = require("./db");
 app.set("view engine", "ejs");
 app.use("/public", express.static("public"));
 
+app.get("/myinfo/:id_prof", async (req, res) => {
+  const { id_prof } = req.params;
+  console.log(id_prof);
+  const profile = await db("profile")
+    .select("username", "phone_number", "job_title", "email", "birth_date")
+    .where({
+      id_prof: id_prof,
+    });
+  res.render("profile", { profile });
+});
+
 app.get("/myinfo", async (req, res) => {
   //   const profile = await db("profile").select("*");
   const profile = await db("profile")
@@ -14,6 +25,29 @@ app.get("/myinfo", async (req, res) => {
   res.render("profile", { profile });
 });
 
+app.get("/offDays", async (req, res) => {
+  const dayoff = await db("dayoff")
+    .join(
+      "type_name",
+      "typeoff.id_type",
+      "=",
+      "dayoff.id_type",
+      "username",
+      "profile.id_acc",
+      "=",
+      "dayoff.id_acc"
+    )
+    // .join("username", "profile.id_acc", "=", "dayoff.id_acc")
+    .select(
+      "profile.username",
+      "dayoff.date",
+      "dayoff.reason",
+      "dayoff.status",
+      "typeoff.type_name"
+    );
+  console.log(dayoff);
+  res.render("dayoff", { dayoff });
+});
 app.listen(5000, () => {
   console.log("Server on board");
 });
